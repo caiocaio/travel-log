4c614cd405d11bf0128d29439d1a825189d5bf15
Caio Marquez <caiomchaves@gmail.com>
2021-02-08 17:37:59 -0300

    08/02/2021 seg

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,29 @@
-/* eslint-disable linebreak-style */
 const express = require('express');
 const morgan = require('morgan');
-const helmet = require("helmet")
+const helmet = require('helmet');
+const cors = require('cors');
+const middlewares = require('./middlewares');
 
 const app = express();
 app.use(morgan('common'));
+app.use(helmet());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'It`s working!!',
+  });
+});
+
+// notfound middleware
+app.use(middlewares.notfound);
+
+// error handle middleware
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
